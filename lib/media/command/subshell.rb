92abfb3c85e8645333699db630d7fe1421e8b1e6
Jamie Hodge <jamiehodge@me.com>
2013-06-20 08:44:02 +0200

    use IO.select and IO.read_nonblock

--- lib/media/command/subshell.rb
+++ lib/media/command/subshell.rb
@@ -5,41 +5,37 @@ module Media
   module Command
     class Subshell
       extend Forwardable
-      def_delegators :status, :pid, :exitstatus, :termsig, :stopsig
+      def_delegators :'wait_thread.value', :pid, :exitstatus, :termsig, :stopsig
     
-      attr_reader :out, :error, :status
+      attr_reader :out, :error, :wait_thread
     
       def initialize(args)
         @cmd = Array(args.fetch(:cmd))
+        @out = ''
+        @error = ''
       end
     
       def call
         ENV['CLICOLOR'] = nil
         ENV['AV_LOG_FORCE_COLOR'] = nil
         
-        @out, @error, @status = Open3.popen3(*@cmd) {|stdin,stdout,stderr,thread|
-          
-          out = Thread.new do
-            stdout.each_with_object([]) do |line, memo|
-              memo << line
-              yield line.strip if block_given?
-            end.join
-          end
-          
-          err = Thread.new do
-            stderr.each_with_object([]) do |line, memo|
-              memo << line
-              yield line.strip if block_given?
-            end.join
-          end
-          
-          stdin.close
-          
-          [out.value, err.value, thread.value]
-        }
+        _, o, e, @wait_thread = Open3.popen3(*@cmd)
+        
+        while wait_thread.alive?
+          next unless IO.select([o, e], nil, nil, 1)
+          out << read(io: o)
+          error << read(io: e).tap {|e| yield e.strip if block_given? }
+        end
+        
         self
+      end  
+        
+      def read(io:, buffer: 65536)
+        io.read_nonblock(buffer)
+      rescue EOFError, IO::WaitReadable
+        ''
       end
-    
+          
       def success?
         exitstatus == 0
       end
